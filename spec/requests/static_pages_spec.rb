5a2c0a1855ab14051e45286b7cbf0c746b15ab06
Antriksh <antrix.sharma@gmail.com>
2013-07-21 22:12:35 -0400

    Minor Title Changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Micropost Web-App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
