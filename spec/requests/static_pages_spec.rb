452148d6aca8350d77f6992a140f2bc310170739
Antriksh <antrix.sharma@gmail.com>
2013-07-22 15:05:42 -0400

    Add title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,17 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Micropost Web-App')
     end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Micropost Web-App')
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+
   end
 
   describe "Help page" do
